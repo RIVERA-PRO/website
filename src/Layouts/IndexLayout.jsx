a96ad2e9dbdd70067fb41cc3b283857e72a39b7c
Juan Rivera <indiojuan2012.jr@gmail.com>
2023-04-22 23:02:24 -0300

    butn

--- src/Layouts/IndexLayout.jsx
+++ src/Layouts/IndexLayout.jsx
@@ -4,6 +4,7 @@ import Hero from '../Components/Hero/Hero'
 import Footeer from '../Pages/Footeer/Footeer'
 import AboutPage from '../Pages/AboutPage/AboutPage'
 import Projects from '../Components/Projects/Projects'
+import ButonScroll from '../Components/ButonScroll/ButonScroll'
 export default function IndexLayout() {
     return (
         <div className='fondo'>
@@ -11,6 +12,7 @@ export default function IndexLayout() {
             <Hero />
             <AboutPage />
             <Projects />
+            <ButonScroll />
             <Footeer />
 
         </div>
